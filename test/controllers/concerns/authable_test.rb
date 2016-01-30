d5c4cbdfc27d7a28857fbafaf33e246d9ab81851
Mohamad El-Husseini <husseini.mel@gmail.com>
2016-01-30 11:32:05 -0200

    fixes authable test

--- test/controllers/concerns/authable_test.rb
+++ test/controllers/concerns/authable_test.rb
@@ -7,14 +7,27 @@ end
 class AuthableTest < ActionController::TestCase
   setup do
     include_default_request_headers
-    @user = create(:user)
-    request.headers['Authorization'] = @user.auth_token
     @dummy = DummyController.new
   end
 
   test "#current_user returns the use from the authorization header" do
+    user = create(:user)
+    request.headers['Authorization'] = user.auth_token
+
     @dummy.stub(:request, request) do
-      assert_equal @user.auth_token, @dummy.current_user.auth_token
+      assert_equal user.auth_token, @dummy.current_user.auth_token
     end
+  end
+
+  test "#authenticate_with_token!" do
+    request.headers['Authorization'] = :fake_token
+
+    @dummy.stub(:request, request) do
+      @dummy.instance_variable_set(:@_response, response)
+      @dummy.authenticate_with_token!
+    end
+
+    assert_equal "Not authenticated", json_response_body[:errors]
+    assert_response :unauthorized
   end
 end
